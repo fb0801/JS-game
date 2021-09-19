46fa34c7cb77133a3c6c903cd332289f3188bd53
Farhan <74499472+fb0801@users.noreply.github.com>
2021-09-19 13:10:10 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ var block = document.getElementById("block");
 
 function jump(){
     //to access the character and make them jump
+
+    //if statement to prevent the user from spamming the jump button
     if (character.classList != "animation"){
         character.classList.add('animate');
     }
@@ -13,3 +15,18 @@ function jump(){
 
 
 }
+
+//check if the user has hit the block to end the game
+var checkDead = setInterval(function(){
+    var characterTop = 
+    parseInt(window.getComputedStyle(character).getPropertyValue('top'));
+
+        var blockLeft = 
+        parseInt(window.getComputedStyle(block).getPropertyValue('left'));
+        if(blockLeft < 20 && blockLeft > 0 && characterTop >=130){
+            block.style.animation = "none";
+            block.style.display = "none";
+            alert("You lose");
+        }
+    
+},10);
