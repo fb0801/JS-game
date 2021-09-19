858e803eb0496255a4d4808370977c9344115c15
Farhan <74499472+fb0801@users.noreply.github.com>
2021-09-19 12:20:27 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var character =
+document.getElementById("character");
+var block = document.getElementById("block");
+
+function jump(){
+    //to access the character and make them jump
+    character.classList.add('animate');
+
+
+}
